5a5686e86409758294d9ba8aa150244d7c614851
carsonmagnuson <carsonmagnuson@gmail.com>
2026-07-17 20:54:56 -0400

    add schema changes

--- roborean/db/schema.ts
+++ roborean/db/schema.ts
@@ -1,7 +1,8 @@
-import { pgTable, pgTableCreator } from "drizzle-orm/pg-core";
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
 export const words = pgTable("words", {
-
-
-
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  word: text("word").notNull().unique(),
+  meaning: text("meaning").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
 });
