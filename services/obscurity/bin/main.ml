1a9800b7c7096426d3618cbe43619304e3598176
carsonmagnuson <carsonmagnuson@gmail.com>
2026-07-20 03:26:02 -0400

    stand up test service

--- services/obscurity/bin/main.ml
+++ services/obscurity/bin/main.ml
@@ -1,1 +1,10 @@
-let () = print_endline "Hello, World!"
+let () = 
+  Dream.run ~interface:"0.0.0.0" ~port:8080
+  @@ Dream.logger
+  @@ Dream.router [
+    Dream.get "/" (fun _req ->
+      Dream.html "obscurity service lives");
+    Dream.get "/score/:word" (fun req ->
+      let word = Dream.param req "word" in
+      Dream.json (Printf.sprintf {|{"word": "%s", "score":null}|} word));
+  ]
