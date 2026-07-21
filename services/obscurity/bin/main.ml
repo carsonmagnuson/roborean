7360c9f700682f970d1457bd01f90b808ec43906
carsonmagnuson <carsonmagnuson@gmail.com>
2026-07-21 05:51:17 -0400

    add ocaml endpoint for word counts in freq table

--- services/obscurity/bin/main.ml
+++ services/obscurity/bin/main.ml
@@ -1,10 +1,19 @@
+let table = Obscurity.load_table "data/count_1w.txt"
 let () = 
   Dream.run ~interface:"0.0.0.0" ~port:8080
   @@ Dream.logger
   @@ Dream.router [
     Dream.get "/" (fun _req ->
       Dream.html "obscurity service lives");
+
     Dream.get "/score/:word" (fun req ->
       let word = Dream.param req "word" in
-      Dream.json (Printf.sprintf {|{"word": "%s", "score":null}|} word));
+      let response =
+        match Hashtbl.find_opt table word with
+        | Some count -> Printf.sprintf {|{"word": "%s", "count": %d}|} word count
+        | None -> Printf.sprintf {|{"word": "%s", "count": null}|} word
+      in
+      Dream.json response);
   ]
+
+
