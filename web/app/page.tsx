9c0359276dd0428d9c985420a5340e1002657efc
carsonmagnuson <carsonmagnuson@gmail.com>
2026-07-27 08:46:11 -0400

    fix(mobile): modify H1 header classname attr for mobile responsivenes

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,8 +2,8 @@ import WordGame from "./word-game";
 
 export default function Home() {
   return (
-    <main className="max-w-2xl mx-auto p-8">
-      <h1 className="text-8xl font-serif mb-2">ROBOREAN</h1>
+    <main className="max-w-2xl mx-auto p-4 sm:p-8">
+      <h1 className="text-6xl sm:text-6xl md:text-8xl font-serif mb-2">ROBOREAN</h1>
       <p className="text-sm text-gray-500 mb-6 justify-center">
         How obscure is your vocabulary?
       </p>
@@ -11,3 +11,4 @@ export default function Home() {
     </main>
   );
 }
+
